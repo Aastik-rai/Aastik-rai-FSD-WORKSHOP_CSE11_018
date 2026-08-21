edf72efff0c117baaa371fe2f61d9c8261c6e4e3
Aastik-rai <aastik.25b01010279@abes.ac.in>
2026-08-21 14:22:22 +0530

    assignment

---
+++ FSD_WORKSHOP/backend/Lab-3/httpserver.js
@@ -0,0 +1,8 @@
+import http, { createServer } from "http"
+const server=http.createServer((req,res)=>{
+    
+
+})
+server.listen(3000,()=>{
+    console.log("server is running in port number 3000")
+})
