c877f575395996661e05a9e7f494acafe9259045
Aastik-rai <aastik.25b01010279@abes.ac.in>
2026-09-03 16:07:12 +0530

    3sept-2026

--- httpserver.js
+++ httpserver.js
@@ -4,21 +4,25 @@ const port = 3000;
 
 const users = [
     {
+        id: 1,
         name: "Aastik Rai",
         phone: "7827536087",
         email: "aastik@gmail.com"
     },
     {
+        id: 2,
         name: "Aastik Rai",
         phone: "7827536087",
         email: "aastik@gmail.com"
     },
     {
+        id: 3,
         name: "Aastik Rai",
         phone: "7827536087",
         email: "aastik@gmail.com"
     },
     {
+        id: 4,
         name: "Aastik Rai",
         phone: "7827536087",
         email: "aastik@gmail.com"
@@ -30,7 +34,6 @@ const server = http.createServer((req, res) => {
     const url = req.url;
     const method = req.method;
 
-    
     if (url === "/msg" && method === "GET") {
 
         res.statusCode = 200;
@@ -39,7 +42,6 @@ const server = http.createServer((req, res) => {
         res.end("Hello World");
     }
 
-  
     else if (url === "/sys" && method === "GET") {
 
         res.statusCode = 404;
@@ -48,7 +50,7 @@ const server = http.createServer((req, res) => {
         res.end("Page not found");
     }
 
-   
+    // GET all users
     else if (url === "/users" && method === "GET") {
 
         res.statusCode = 200;
@@ -57,34 +59,51 @@ const server = http.createServer((req, res) => {
         res.end(JSON.stringify(users));
     }
 
-    
+    // GET user by id
+    else if (url.startsWith("/users/") && method === "GET") {
+
+        const id = url.split("/")[2];
+
+        const user = users.find((u) => u.id == id);
+
+        if (!user) {
+            return res.end("User not found");
+        }
+
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+
+        res.end(JSON.stringify(user));
+    }
+
+    // CREATE user
     else if (url === "/create" && method === "POST") {
 
         let body = "";
 
         req.on("data", (content) => {
-            body = body + content;
+            body+=content;
         });
-        const newUser = JSON.parse(body);
-        users.push(newUser);
-        res.write(JSON.stringify({
-                    user:newUser
-        }));
-        
 
-               
+        req.on("end", () => {
 
+            const newUser = JSON.parse(body);
 
-        
-        
+            users.push(newUser);
 
-                
-               
+            res.statusCode = 201;
+            res.setHeader("Content-Type", "application/json");
 
-           
+            res.end(JSON.stringify({
+                user: newUser
+            }));
+        });
     }
 
-   
+    else if(url==="/update" && method==="PUT"){
+        
+    }
+
     else {
 
         res.statusCode = 404;
